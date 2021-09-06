37c9769784ae201125f7ca1ed4a9f777ec95d50c
jgone6 <qkr2867@naver.com>
2021-09-06 14:58:03 +0900

    ballbounce

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -7,8 +7,6 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]
     private float Speed = 10f;
 
-    public float GetSpeed => Speed;
-
     Rigidbody rigidbody;
     Vector3 movement;
     float h, v;
@@ -30,14 +28,9 @@ public class PlayerMove : MonoBehaviour
         movement = movement.normalized * Speed * Time.deltaTime;
         rigidbody.MovePosition(transform.position + movement);
 
-        anim.SetBool("Run", v > 0);
-        anim.SetBool("RunLeft", h < 0);
-        anim.SetBool("RunRight", h > 0);
-        anim.SetBool("RunBack", v < 0);
-        
         if (Input.GetMouseButtonDown(0))
         {
-            anim.SetTrigger("Attack");
+            
         }
     }
 }
